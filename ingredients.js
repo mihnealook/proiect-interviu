ec9f504c4e9f84df2dc066119a4f84a3278a8800
mihnealook <mihnealook@gmail.com>
2021-10-21 10:14:06 +0300

    preluat automat ingrediente ex2

--- ingredients.js
+++ ingredients.js
@@ -6,11 +6,48 @@ function getIngredients() {
     let searchQuerry = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list";
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        for(var ingredient of JSON.parse(this.responseText)["drinks"]) {
+        for(let ingredient of JSON.parse(this.responseText)["drinks"]) {
             allIngredients.push(ingredient["strIngredient1"]);
         };
-        console.log(allIngredients); 
+        addIngredientsToList()
     };
     xhttp.open("GET", searchQuerry, true);
     xhttp.send();
+}
+
+function addIngredientsToList() {
+    const wantedIngredients = document.getElementById("butoane-dorite");
+    const unwantedIngredients = document.getElementById("butoane-nedorite");
+    for(let ingredient of allIngredients) {
+        wantedIngredients.innerHTML +=
+        `<input type="checkbox" class="btn-check mb-5 me-2" id="btn-dorit${allIngredients.indexOf(ingredient)}" autocomplete="off">
+        <label class="btn-sm btn-outline-info" for="btn-dorit${allIngredients.indexOf(ingredient)}">${ingredient}</label>` + "\n";
+
+        unwantedIngredients.innerHTML +=
+        `<input type="checkbox" class="btn-check mb-5 me-2" id="btn-nedorit${allIngredients.indexOf(ingredient)}" autocomplete="off">
+        <label class="btn-sm btn-outline-danger" for="btn-nedorit${allIngredients.indexOf(ingredient)}">${ingredient}</label>` + "\n";
+    }
+}
+
+function cautaCocktail() {
+    const wantedIngredients = [];
+    const unwantedIngredients = [];
+
+    for(let i = 0; i < allIngredients.length; i++) {
+        let checkedWanted = document.getElementById("btn-dorit"+i.toString()).checked;
+        let checkedUnwanted = document.getElementById("btn-nedorit"+i.toString()).checked;
+
+        if(checkedWanted && checkedUnwanted) {
+            alert("Nu poți bifa un element și ca dorit și ca nedorit!!");
+            return;
+        } else {
+            if(checkedWanted)
+                wantedIngredients.push(allIngredients[i]);
+            if(checkedUnwanted)
+            unwantedIngredients.push(allIngredients[i]);
+        }
+    }
+
+    console.log(wantedIngredients);
+    console.log(unwantedIngredients);
 }
